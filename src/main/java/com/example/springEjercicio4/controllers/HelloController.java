286a46b55724b680b46ff613c18d4e31c8cf0948
Hector <hectorpose@gmail.com>
2022-06-29 19:50:55 +0000

    created jar

--- src/main/java/com/example/springEjercicio4/controllers/HelloController.java
+++ src/main/java/com/example/springEjercicio4/controllers/HelloController.java
@@ -1,13 +1,17 @@
 package com.example.springEjercicio4.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${app.message}")
+    String message;
     @GetMapping("/hola")
     public String saludo(){
+        System.out.println(message);
         return "Hola mundo!!!!!";
     }
 
